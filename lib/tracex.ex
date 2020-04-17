bd41a416ac4c2ccc320c6e0b269795a80f697d22
Michał Szajbe <michal.szajbe@gmail.com>
2020-04-17 21:24:14 +0200

    Initial commit

---
+++ lib/tracex.ex
@@ -0,0 +1,18 @@
+defmodule Tracex do
+  @moduledoc """
+  Documentation for Tracex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tracex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tracex_test.exs
@@ -0,0 +1,8 @@
+defmodule TracexTest do
+  use ExUnit.Case
+  doctest Tracex
+
+  test "greets the world" do
+    assert Tracex.hello() == :world
+  end
+end
