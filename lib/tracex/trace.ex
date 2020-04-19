9d7e74fc3eeae89937f6cdf7a01b756cae0dbbb4
Michał Szajbe <michal.szajbe@gmail.com>
2020-04-19 12:39:31 +0200

    Move trace to correct dir

--- lib/tracex/trace.ex
+++ lib/tracex/trace.ex
@@ -2,7 +2,11 @@ defmodule Tracex.Trace do
   alias Tracex.Event
 
   def inbound?({event, _env}, module) do
-    Event.get_module(event) == module
+    case event do
+      {:local_function, _, _, _} -> false
+      {:local_macro, _, _, _} -> false
+      _ -> Event.get_module(event) == module
+    end
   end
 
   def outbound?({event, env}, module) do
