b91ea9fbc536518412867f45c1e342d4a5c973bb
Michał Szajbe <michal.szajbe@gmail.com>
2020-04-29 20:58:09 +0200

    Add docs and package info to mix.exs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,17 @@
 defmodule Tracex.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :tracex,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -21,8 +25,27 @@ defmodule Tracex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "Tracex",
+      extras: ["README.md"],
+      source_url: "https://github.com/szajbus/tracex",
+      source_ref: @version
+    ]
+  end
+
+  defp package() do
+    [
+      description: "Static analysis for mix projects using compiler tracing.",
+      maintainers: ["Michał Szajbe"],
+      licenses: ["MIT"],
+      links: %{
+        "Github" => "https://github.com/szajbus/tracex"
+      }
     ]
   end
 end
